1468160685cd691d8b729fc8c17537ee3a77c635
rkritchat <rkritchat@gmail.com>
2020-06-28 23:54:45 +0700

    create constant

---
+++ pkg/constant/constant.go
@@ -0,0 +1,18 @@
+package constant
+
+const (
+	//vault-client config
+	VaultURL     = "VAULT.URL"
+	VaultPath    = "VAULT.PATH"
+	VaultToken   = "VAULT.TOKEN"
+	VaultDisable = "VAULT.DISABLE"
+)
+
+const (
+	//common
+	True        = "true"
+	False       = "false"
+	Empty       = ""
+	Space       = " "
+	XVaultToken = "X-Vault-Token"
+)
